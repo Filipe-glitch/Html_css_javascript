8ff114dafaad9e765c2abf38b6e9d3b3d9787b08
Filipe <mrocha.filipe1@gmail.com>
2025-06-24 10:55:58 -0300

    adicionando novas pastas

---
+++ java_codes/test.js
@@ -0,0 +1,8 @@
+console.log("Deus");
+console.log("olá de novo");
+let primeiroNome;
+let nome1 = "eu";
+console.log(typeof nome1);
+idade = 45;
+idade = idade + 6; // essa prevalece pois foi a ultima
+let mensagem = `olá $
